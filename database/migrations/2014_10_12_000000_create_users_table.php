273ff7fa84072a5fc99e8f8ef6201def59b00048
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-29 16:39:03 +0200

    add apartments table add apartments migration add apartment model modify user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->required();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('slug')->unique()->required();
+            $table->string('password')->required();
+            $table->char('name', 50)->nullable();
+            $table->char('lastname', 50)->nullable();
+            $table->date('date_of_birth')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_09_29_141557_create_apartments_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+
+            $table->string('title')->required();
+            $table->string('slug')->unique()->required();
+            $table->text('description')->required();
+            $table->tinyInteger('n_rooms')->required()->unsigned();
+            $table->tinyInteger('n_beds')->required()->unsigned();
+            $table->tinyInteger('n_bathrooms')->required()->unsigned();
+            $table->float('n_square_meters', 6, 2)->required();
+            $table->text('image')->required();
+            $table->tinyInteger('visibility')->default(1);
+            $table->float('daily_price', 6, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
