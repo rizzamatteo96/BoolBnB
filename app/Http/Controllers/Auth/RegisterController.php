455bd6ebac8cb1499ddb11f1a8fb2cfe3f543af1
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-30 12:33:46 +0200

    Fix Register controller add new user data and manage them in register controller add fillable in user model

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Support\Str;
+
 class RegisterController extends Controller
 {
     /*
@@ -50,9 +52,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'max:50'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'lastname' => ['nullable', 'max:50'],
+            'date_of_birth' => ['nullable', 'date'],
         ]);
     }
 
@@ -64,10 +68,32 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        // create and check slug
+        $slug = Str::slug(strstr($data['email'], '@', true), '-'); 
+
+        // copy slug base in slug variable to check if already exist
+        $newEditSlug = $slug;
+
+        // prepare the counter
+        $counter = 0;
+
+        // check if slug already exist and add counter in case of double presence
+        while(User::where('slug', $newEditSlug)->first()){
+            $counter++;
+            $newEditSlug = $slug . '-' . $counter;
+        }
+
+        // save edited slug in main variable before send it to DB
+        $slug = $newEditSlug;
+
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'lastname' => $data['lastname'],
+            'date_of_birth' => $data['date_of_birth'],
+            'slug' => $slug,
         ]);
     }
 }
--- app/User.php
+++ app/User.php
@@ -16,7 +16,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email', 
+        'slug',
+        'password',
+        'name', 
+        'lastname', 
+        'date_of_birth'
     ];
 
     /**
