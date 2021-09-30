ee9687f7a3d290b0f936c61a70ced2915f043194
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-30 20:56:47 +0200

    add sponsorship table seeder remove Apartment_Sponsorhip model for wrong name add Apartment_Sponsorship model

--- app/Apartment_Sponsorship.php
+++ app/Apartment_Sponsorship.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Apartment_Sponsorhip extends Model
+class Apartment_Sponsorship extends Model
 {
     //
 }
---
+++ database/seeds/SponsorshipTable.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Sponsorship;
+
+class SponsorshipTable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Sponsorships details array
+        $sponsorShips = [
+            [
+                'type' => 'basic',
+                'price' => 2.99,
+                'duration' => 24,
+            ],
+            [
+                'type' => 'standard',
+                'price' => 5.99,
+                'duration' => 72,
+            ],
+            [
+                'type' => 'premium',
+                'price' => 9.99,
+                'duration' => 144,
+            ],
+        ];
+
+        foreach($sponsorShips as $item){
+            $newSponsorship = new Sponsorship();
+            $newSponsorship->type = $item['type'];
+            $newSponsorship->price = $item['price'];
+            $newSponsorship->duration = $item['duration'];
+            $newSponsorship->save();
+        }
+    }
+}
