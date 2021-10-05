8257d03519f8c0a9cf40b2b729eda04a5422fbab
cromeo98 <romeo.christian98@gmail.com>
2021-10-05 10:19:20 +0200

    add validation-registration js & webpackmix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,12 +16,6 @@
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"  autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
 
