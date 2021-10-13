a806e27f75cca90ec3f229829a167f2f9113543c
cromeo98 <romeo.christian98@gmail.com>
2021-10-13 12:32:25 +0200

    fix foreach with forelse

--- resources/views/userreg/messages/index.blade.php
+++ resources/views/userreg/messages/index.blade.php
@@ -11,22 +11,20 @@
     @foreach ($apartments as $apartment)
         <div>Nome appartamento: {{$apartment->title}}</div>
             @foreach ($messages_list as $message)
-                @if (!empty($message))
-                    @foreach ($message as $item)
-                        @if($item['apartment_id'] == $apartment->id)
-                            <ul>
-                                @foreach ($item as $key => $message_detail)
-                                    @if ($key == 'description')
-                                        <li>Messaggio: {{$message_detail}}</li>
-                                    @endif
-                                    @if ($key == 'email') 
-                                        <li>Email: {{$message_detail}}</li>
-                                    @endif
-                                @endforeach
-                            </ul>
-                        @endif 
-                    @endforeach
-                @endif
+                @forelse ($message as $item)
+                    @if($item['apartment_id'] == $apartment->id)
+                        <ul>
+                            @foreach ($item as $key => $message_detail)
+                                @if ($key == 'description')
+                                    <li>Messaggio: {{$message_detail}}</li>
+                                @endif
+                                @if ($key == 'email') 
+                                    <li>Email: {{$message_detail}}</li>
+                                @endif
+                            @endforeach
+                        </ul>
+                    @endif 
+                @endforeach
             @endforeach
     @endforeach
 
