37b03f3b9aae30968b9097ed591c6a1d28596931
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-10-18 17:23:48 +0200

    add stat seeder and graph

---
+++ database/seeds/StatisticsTableSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Apartment;
+use App\Statistic;
+
+class StatisticsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (Apartment::all() as $apartment){
+            
+            $randNum = rand(200, 1000);
+            for ($i = 0; $i < $randNum; $i++) { 
+                
+                // create new stat
+                $newStat = new Statistic();
+    
+
+                // set apartment id
+                $newStat->apartment_id = $apartment->id;
+    
+
+                // create random date
+                //day
+                //Start point of our date range.
+                $startDay = strtotime("14 April 2021");
+
+                //End point of our date range.
+                $endDay = strtotime("22 October 2021");
+
+                //Custom range.
+                $timestamp = mt_rand($startDay, $endDay);
+
+                //Print it out.
+                $newStat->clicked_at = date("Y-m-d", $timestamp);
+
+
+                // set visitor
+                $newStat->visitor = mt_rand(0, 254) . "." . mt_rand(0, 254) . "." . mt_rand(0, 254) . "." . mt_rand(0, 254);
+    
+                
+                // Save new statistic
+                $newStat->save();
+                
+            }
+
+        };
+    }
+}
--- resources/views/userreg/statistics/show.blade.php
+++ resources/views/userreg/statistics/show.blade.php
@@ -12,9 +12,60 @@
 		<h5 class="card-title">Statistiche appartamento</h5>
 		<p class="card-text">L'appartamento è stato visualizzato {{count($statistics)}} volte</p>
 	</div>
-	{{-- <div class="card-footer">
-			<small class="text-muted">Inviato il:</small>
-	</div> --}}
-</div>	
+</div>
+
+<div class="card bg-light mb-3">
+	<div class="row card-body">
+		{{-- Gennaio --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Febbraio --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Marzo --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Aprile --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Maggio --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Giugno --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Luglio --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Agosto --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Settembre --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Ottobre --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Novembre --}}
+		<div class="col-1 border border-primary"></div>
+
+		{{-- Dicembre --}}
+		<div class="col-1 border border-primary"></div>
+	</div>
+	<div class="row card-body">
+		<div class="col-1">Gen</div>
+		<div class="col-1">Feb</div>
+		<div class="col-1">Mar</div>
+		<div class="col-1">Apr</div>
+		<div class="col-1">Mag</div>
+		<div class="col-1">Giu</div>
+		<div class="col-1">Lug</div>
+		<div class="col-1">Ago</div>
+		<div class="col-1">Set</div>
+		<div class="col-1">Ott</div>
+		<div class="col-1">Nov</div>
+		<div class="col-1">Dic</div>
+	</div>
+</div>
 
 @endsection
