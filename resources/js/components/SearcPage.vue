501ba6b201db0a340589bd176451d28ae10c541b
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-10-20 15:57:24 +0200

    fix img in search page

--- resources/js/components/SearcPage.vue
+++ resources/js/components/SearcPage.vue
@@ -341,15 +341,20 @@
                 // min-height: 650px;
 
                 .container-box--img {
-                    width: 100%;
+                    // width: 100%;
                     height: 250px;
                     line-height: 250px;
+                    position: relative;
                     // height: 30%;
                     // border-radius: 10px;
 
                     img {
-                        max-height: 250px;
-                        width: 100%;
+                        position: absolute;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -50%);
+                        max-height: 100%;
+                        max-width: 100%;
                         border-radius: 10px;
                         box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
                         vertical-align: middle;
